1d1f45f7bd9cd70f70eac0336b470ef8e659af88
tyoisu <tyoisu@tyoisunoiMac.local>
2019-03-19 15:44:08 +0900

    exam math finish

--- toeic/once.php
+++ toeic/once.php
@@ -4,6 +4,14 @@ $point = 0;
 $listening_point = 0;
 $reading_point = 0;
 
+$home = $_POST["home"];
+
+if (!empty($home)) {
+    header("location: main.php");
+    exit();
+}
+
+
 function question($num,$choice){
     for($i = 1; $i <= $num; $i++){
         if(rand(1,$choice) == 1){
@@ -14,28 +22,40 @@ function question($num,$choice){
 }
 
 //リスニングpart1(写真描写問題) 6問 4択
-$listening_point = $listening_point + question(6,4);
+$listening_point1 = question(6,4);
+$listening_point = $listening_point + $listening_point1;
+
 //リスニングpart2(応答問題) 25問 3択
-$listening_point = $listening_point + question(25,3);
+$listening_point2 = question(25,3);
+$listening_point = $listening_point + $listening_point2;
+
 //リスニングpart3(会話問題) 39問 4択
-$listening_point = $listening_point + question(39,4);
+$listening_point3 = question(39,4);
+$listening_point = $listening_point + $listening_point3;
+
 //リスニングpart4(説明文問題) 30問 4択
-$listening_point = $listening_point + question(30,4);
-echo $listening_point . '/';
+$listening_point4 = question(30,4);
+$listening_point = $listening_point + $listening_point4;
+
 
 //リーディングpart5(短文穴埋め問題) 30問 4択
-$reading_point = $reading_point + question(30,4);
+$reading_point5 = question(30,4);
+$reading_point = $reading_point + $reading_point5;
+
 //リーディングpart6(長文穴埋め問題) 16問 4択
-$reading_point = $reading_point + question(16,4);
+$reading_point6 = question(16,4);
+$reading_point = $reading_point + $reading_point6;
+
 //リーディングpart7(長文読解問題) 54問 4択
-$reading_point = $reading_point + question(54,4);
-echo $reading_point . '/';
+$reading_point7 = question(54,4);
+$reading_point = $reading_point + $reading_point7;
+
+
 
 //スコアレンジ
 $listening_score = [5, rand(5, 20), rand(15, 40), rand(20, 55), rand(35, 70), rand(50, 90), rand(70, 105), rand(85, 130),
 rand(110, 155), rand(135, 180), rand(160, 210), rand(190, 235), rand(215, 260), rand(240, 285), rand(265, 315), rand(295, 340), rand(325, 375),
 rand(355, 415), rand(395, 450), rand(435, 490), rand(480, 495)];
-echo $listening_score[1];
 
 $reading_score = [5, rand(5, 20), rand(15, 35), rand(20, 50), rand(30, 65), rand(40, 80), rand(55, 100), rand(75, 120),
 rand(100, 140), rand(120, 170), rand(150, 195), rand(175, 225), rand(205, 255), rand(235, 285), rand(265, 315), rand(295, 345), rand(325, 375),
@@ -43,16 +63,16 @@ rand(355, 400), rand(380, 430), rand(410, 475), rand(460, 495)];
 
 
 for($i = 0; $i <= 4; $i++){
-    if($listening_point + $i % 5 == 0){
-        $listening_point = $listening_point - $i;
+    if(($listening_point + $i) % 5 == 0){
+        $listening_point = $listening_point + $i;
         break;
     }
 }
 $listening_list_num = $listening_point / 5;
 
 for($i = 0; $i <= 4; $i++){
-    if($reading_point + $i % 5 == 0){
-        $reading_point = $reading_point - $i;
+    if(($reading_point + $i) % 5 == 0){
+        $reading_point = $reading_point + $i;
         break;
     }
 }
@@ -88,9 +108,11 @@ $total = $total_listening_point + $total_reading_point;
             </header>
             <div class="main">
                 <h3>結果</h3>
-                <p><?php echo $total ?></p>
+                <p>合計得点:<?php echo $total ?>点</p>
                 <p>リスニング:<?php echo $total_listening_point; ?>点</p>
                 <p>リーディング:<?php echo $total_reading_point; ?>点</p>
+                <input type="submit" name="re_exam" value="再受験">
+                <input type="submit" name="home" value="コース選択">
             </div>
             <footer>
                 <p>© 2019  TOEIC Simulator</p>
