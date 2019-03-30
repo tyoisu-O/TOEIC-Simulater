4b91cebb1f863d9ca9756c41940b25e15f98170d
ota <unban5963@gmail.com>
2019-03-30 17:29:24 +0900

    add main's css

--- toeic/main.php
+++ toeic/main.php
@@ -29,7 +29,11 @@ if (!empty($target) && !empty($_POST["exam_target"])){
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="toeic.css">
     <link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet">
@@ -43,30 +47,38 @@ if (!empty($target) && !empty($_POST["exam_target"])){
                 <p>TOEIC Simulator</p>
             </header>
         </a>
-        <div class="main">
-            <div class="once">
-                <h2>一回受験</h2>
-                <p>通常と同じく、一度受験をします。制限時間は2時間ですが、あなたは一瞬で200問を解くことができる素晴らしいコースです。</p>
-                <input type="submit" name="start_once" value="開始">
-            </div>
-            <div class="number">
-                <h2>受験回数指定</h2>
-                <p>何回も試験を受けたい人におすすめなコース。試行した全ての試験の中から最高点と最低点がわかります。</p>
-                <input type="text" name="exam_number" placeholder="回数">
-                <input type="submit" name=start_number value="開始">
-            </div>
-            <div class="target">
-                <h2>目標点固定</h2>
-                <p>TOEICで獲得したい得点がわかっている人に向いているコース。その点数をたたき出す為に必要な受講回数がわかります。</p>
-                <input type="text" name="exam_target" placeholder="10〜990(点数)">
-                <input type="submit" name="start_target" value="開始">
-            </div>
-            <br>
-            <div class="attention">
-                <p>※このTOEICシュミレーターは正確な得点を表示することはできません。TOEIC公式のスコアレンジに基づき、計算をしています。<br>また過度な回数、目標点は処理落ちの原因になりますのでご注意ください。</p>
-            </div>
-            <div class="koukoku">
-                <P>空きスペース</P>
+        <div class="background">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="main">
+                        <div class="exam_method">
+                            <div class="once col-3">
+                                <h2>一回受験</h2>
+                                <p>通常と同じく、一度受験をします。制限時間は2時間ですが、あなたは一瞬で200問を解くことができる素晴らしいコースです。</p>
+                                <input type="submit" name="start_once" value="開始">
+                            </div>
+                            <div class="number col-3">
+                                <h2>受験回数指定</h2>
+                                <p>何回も試験を受けたい人におすすめなコース。試行した全ての試験の中から最高点と最低点がわかります。</p>
+                                <input type="text" name="exam_number" placeholder="回数">
+                                <input type="submit" name=start_number value="開始">
+                            </div>
+                            <div class="target col-3">
+                                <h2>目標点固定</h2>
+                                <p>TOEICで獲得したい得点がわかっている人に向いているコース。その点数をたたき出す為に必要な受講回数がわかります。</p>
+                                <input type="text" name="exam_target" placeholder="10〜990(点数)">
+                                <input type="submit" name="start_target" value="開始">
+                            </div>
+                        </div>
+                        <br>
+                        <div class="attention">
+                            <p>※このTOEICシュミレーターは正確な得点を表示することはできません。TOEIC公式のスコアレンジに基づき、計算をしています。<br>また過度な回数、目標点は処理落ちの原因になりますのでご注意ください。</p>
+                        </div>
+                        <div class="koukoku">
+                            <P>空きスペース</P>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <footer>
